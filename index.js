a557e3d98e254b9a26f268ceaca4993cfc255c03
Momoleking55 <momoleking55@gmail.com>
2026-03-30 19:22:56 +0200

    mysql url v2

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
-const connection = mysql.createConnection(process.env.MYSQL_URL);
+const connection = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
+});
 
 
 connection.connect((err) => {
